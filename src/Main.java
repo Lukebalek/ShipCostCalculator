6606be72f24289d38509ca2c7d7926b4388aedc6
Lukebalek <Lukebalek23@gmail.com>
2023-09-28 15:35:21 -0400

    ShipCost

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Prompt the user for the item price
         System.out.print("Enter the price of the item: $");
         double itemPrice = scanner.nextDouble();
 
-        // Calculate shipping cost
         double shippingCost;
         if (itemPrice >= 100.0) {
-            shippingCost = 0.0; // Free shipping for items $100 or more
+            shippingCost = 0.0;
         } else {
-            shippingCost = 0.02 * itemPrice; // 2% shipping cost for items less than $100
+            shippingCost = 0.02 * itemPrice;
         }
 
-        // Calculate total price
         double totalPrice = itemPrice + shippingCost;
 
-        // Display shipping cost and total price
-        System.out.printf("Shipping Cost: $%.2f%n", shippingCost);
-        System.out.printf("Total Price: $%.2f%n", totalPrice);
+        System.out.println("Your total cost is " + totalPrice);
+        System.out.println("Your total cost is " + totalPrice);
 
         scanner.close();
     }
